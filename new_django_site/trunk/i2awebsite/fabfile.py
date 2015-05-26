1bdf7730cad6829032bf26cb565edcb41c1690e3
proman <proman@5694d5b0-c0fd-4a05-a1dc-3df563e12c5e>
2015-05-26 13:08:58 +0000

    added fabfile
    git-svn-id: https://pm.i2asolutions.com/svn/i2awebsite@131 5694d5b0-c0fd-4a05-a1dc-3df563e12c5e

---
+++ new_django_site/trunk/i2awebsite/fabfile.py
@@ -0,0 +1,42 @@
+from __future__ import with_statement
+import datetime
+import time
+from fabric.api import local, run, cd, put, sudo, env, show, prefix
+
+env.disable_known_hosts = True
+
+def _deploy(remote_dir, svn_rev, postfix):
+    svn_rev = svn_rev or str(int(time.time()))
+    remote_dir = remote_dir if remote_dir[-1] != '/' else remote_dir[:-1]
+    postfix = '-'.join(remote_dir.split('/')) + postfix
+ 
+    with show('debug'):
+        local('tar czf /tmp/webapp' + postfix + '.tar.gz * --exclude="*.svn"')
+ 
+    with cd(remote_dir):
+        dirname = 'r-%s' % svn_rev
+        sudo('rm -rf %s; mkdir %s' % (dirname, dirname))
+        put('/tmp/webapp' + postfix + '.tar.gz', '/tmp/')
+        sudo('tar xmzf /tmp/webapp' + postfix + '.tar.gz -C %s/%s' % (remote_dir, dirname))
+
+        sudo('rm -rf webapp')
+        sudo('cp -Rp %s webapp' % dirname)
+        sudo('chown -R www-data: webapp')
+        sudo('echo "%s" > webapp/VERSION' % svn_rev)
+
+def deploy(remote_dir, svn_rev='', nfs=False):
+    """
+    Deploy the app to PROD
+    """
+    _deploy(remote_dir, svn_rev, '-prod')
+    with show('debug'):
+        with cd(remote_dir + '/webapp'):
+            sudo('chmod +x manage.py')
+            with prefix('source /opt/virtualenv/i2awebsite/bin/activate'):
+                sudo('./manage.py collectstatic --noinput')
+            #     sudo('./manage.py migrate --noinput')
+            #     sudo('./manage.py collectstatic --noinput')
+            # sudo('touch capp_django/wsgi.py')
+    #sudo('service i2awebsite restart')
+    return
+
